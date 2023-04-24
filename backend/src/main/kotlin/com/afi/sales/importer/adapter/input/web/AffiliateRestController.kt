34d4a8c86b9f9a4955b6f112c2064b647807d8d7
André de Fontana Ignacio <ignacio83@gmail.com>
2023-04-24 01:43:04 -0300

    docs: Add swagger-ui (#7)
    resolves: T21

--- backend/src/main/kotlin/com/afi/sales/importer/adapter/input/web/AffiliateRestController.kt
+++ backend/src/main/kotlin/com/afi/sales/importer/adapter/input/web/AffiliateRestController.kt
@@ -1,7 +1,13 @@
 package com.afi.sales.importer.adapter.input.web
 
 import com.afi.sales.importer.application.port.input.GetAffiliateBalance
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,10 +16,21 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/affiliate")
+@Tag(name = "affiliate")
 class AffiliateRestController(private val getConsumerBalance: GetAffiliateBalance) {
 
     @GetMapping("/{id}/balance")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(
+        summary = "Gets affiliate balance",
+        responses =
+        [
+            ApiResponse(
+                responseCode = "404",
+                content = [Content(schema = Schema(implementation = ProblemDetail::class))],
+            ),
+        ],
+    )
     fun balance(@PathVariable id: Long) =
         getConsumerBalance.getBalance(id).let(::BalanceResource)
 }
--- backend/src/main/kotlin/com/afi/sales/importer/adapter/input/web/BalanceResource.kt
+++ backend/src/main/kotlin/com/afi/sales/importer/adapter/input/web/BalanceResource.kt
@@ -1,7 +1,10 @@
 package com.afi.sales.importer.adapter.input.web
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 
+@Schema(description = "Balance")
 data class BalanceResource(
+    @Schema(description = "Value", readOnly = true, nullable = false, example = "10.99")
     val value: BigDecimal,
 )
